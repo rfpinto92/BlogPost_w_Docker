95d69e2acb38ca72a6a671255a9f5794d844960b
rfpinto92 <rfpinto92@gmail.com>
2022-07-15 17:51:32 +0100

    Working Version
    Working Version

---
+++ server/config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '595431127588-6ne2mtjkpmsgmeq54kmpdrg663kokecc.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  cookieKey: '123123123',
+  redisUrl: 'redis://redis:6379',
+  mongoURI: 'mongodb://mongo:27017/blog_dev',
+};
+
