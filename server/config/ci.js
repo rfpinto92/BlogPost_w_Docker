d3edaf9a2af2c52494a5456f020b306fe9904351
rfpinto92 <rfpinto92@gmail.com>
2022-07-16 14:57:59 +0100

    some changes
    some changes

--- server/config/ci.js
+++ server/config/ci.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID:
-    '595431127588-6ne2mtjkpmsgmeq54kmpdrg663kokecc.apps.googleusercontent.com',
+    '595431127588-6ne2jhyuvuyq54kmpdrg663kokecc.apps.googleusercontent.com',
   googleClientSecret: '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',
   cookieKey: '123123123',
   redisUrl: 'redis://redis:6379',
